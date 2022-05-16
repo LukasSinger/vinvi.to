ea04bde0fdb2a82753a4c7142653f8c99e6fd56f
Lukas Singer <7627101@isd535.org>
2022-05-16 11:31:20 -0500

    Useless attempt at receiving book cover image

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const { parse } = require("path");
 
 let currentIdTime;
 let currentIds;
@@ -16,7 +17,6 @@ http
 console.log("The server is now listening for requests.");
 
 function handleRequest(req, res) {
-  console.log(req.read());
   let q = url.parse(req.url, true);
   let file;
   if (q.query.title) {
@@ -29,7 +29,8 @@ function handleRequest(req, res) {
     let db = fs.readFileSync("database/database.json");
     if (db.length == 0) db = {};
     else db = JSON.parse(db);
-    db[thisId] = q.query;
+    db[thisId] = parseRequestData(req);
+    console.log(db);
     fs.writeFileSync("database/database.json", JSON.stringify(db));
     servePage("public/pages/index.html", res);
   } else {
@@ -48,5 +49,15 @@ function servePage(file, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(data);
     return res.end();
+  });
+}
+
+function parseRequestData(req) {
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk.toString();
+  });
+  return req.on("end", () => {
+    return parse(body);
   });
 }
