d18b41b9756b9be4beb00264b2283a20a113e906
itshoozi <jeremy@pringlecomputer.com>
2022-05-17 11:29:09 -0500

    Leaving for the day
    Co-authored-by: Mr. Bartucz <jobartucz@rochesterschools.org>
    Co-authored-by: LukasSinger <LukasSinger@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+    content: [
+      'public/pages/**/*.{html,js}',
+    ],
+  theme: {
+    extend: {
+      colors: {
+        accent: 'rgb(132 204 22)',
+      },
+      aspectRatio: {
+        169: '9 / 13',
+      },
+      fontFamily: {
+        titles: ['Righteous', 'cursive'],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+}
