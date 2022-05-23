f3d7af094ad63875ffc74fde1da867805f0e0b07
Lukas Singer <7627101@isd535.org>
2022-05-23 11:37:11 -0500

    Balance + new story reward + cookies

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ const staticPathMappings = {
 };
 
 const NEW_USER_BALANCE = 10;
-  
+const NEW_STORY_REWARD = 5;
+const COST_PER_WORD = 0.01;
+
 const fs = require("fs");
 const formidable = require("formidable");
 const express = require("express");
@@ -24,6 +26,14 @@ const app = express();
 
 let currentIdTime;
 let currentIds;
+
+/**
+ * Counts the words in the provided string.
+ * @returns {number} The number of words.
+ */
+String.prototype.countWords = function () {
+  return this.trim().split(/\s+/).length;
+};
 
 app.use(express.static(PUBLIC_DIR));
 app.engine(".html", require("ejs").__express);
@@ -36,10 +46,7 @@ app
     console.log("The server is now listening for requests.");
   })
   .on("error", (err) => {
-    if (process.env.NODE_ENV)
-      console.log(
-        "You are trying to run the server on an improperly configured machine.\nIf you're just trying to test, use `npm run devstart` instead."
-      );
+    if (process.env.NODE_ENV) console.log("You are trying to run the server on an improperly configured machine.\nIf you're just trying to test, use `npm run devstart` instead.");
     else throw err;
   });
 
@@ -60,6 +67,25 @@ app.post("/api/user/new", (req, res) => {
   servePage("/home", res);
 });
 
+// Handle user balance request
+app.get("/api/user/:id/balance", (req, res) => {
+  let db = getDb();
+  let dbEntry = db.users[req.params.id];
+  if (dbEntry) {
+    authenticateRequest(
+      dbEntry.username,
+      req,
+      db,
+      () => {
+        writeToRes(res, 200, "application/json", dbEntry.balance);
+      },
+      () => {
+        writeToRes(res, 403, "text/html", "Invalid credentials");
+      }
+    );
+  } else writeToRes(res, 404, "text/html", "404");
+});
+
 // Serve story page
 app.get("/story/:id(\\d+)", (req, res) => {
   let dbEntry = getDb().stories[req.params.id];
@@ -72,11 +98,17 @@ app.get("/api/story/:id", (req, res) => {
   let db = getDb();
   let dbEntry = db.stories[req.params.id];
   if (dbEntry) {
-    authenticateRequest(dbEntry.username, req, db, () => {
-      writeToRes(res, 200, "application/json", dbEntry);
-    }, () => {
-      writeToRes(res, 403, "text/html", "Locked to content creator");
-    });
+    authenticateRequest(
+      dbEntry.username,
+      req,
+      db,
+      () => {
+        writeToRes(res, 200, "application/json", dbEntry);
+      },
+      () => {
+        writeToRes(res, 403, "text/html", "Locked to content creator");
+      }
+    );
   } else writeToRes(res, 404, "text/html", "404");
 });
 
@@ -99,8 +131,13 @@ app.post("/api/story/new", (req, res) => {
           if (err) throw err;
         });
       }
+      // Calculate cost
+      fields.cost = fields.content.length;
       // Store the metadata and story content
       db.stories[id] = fields;
+      // Add balance to user's account
+      db.users[fields.username].balance += NEW_STORY_REWARD;
+      // Write to database
       fs.writeFile(DB_LOCATION, JSON.stringify(db), (err) => {
         if (err) throw err;
       });
