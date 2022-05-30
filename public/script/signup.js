2b2cfe9268f9c24ba8807426b8ef9d8961d8a169
Lukas Singer <56053273+LukasSinger@users.noreply.github.com>
2022-05-30 09:50:37 -0500

    Signup script

---
+++ public/script/signup.js
@@ -0,0 +1,9 @@
+let formElement = document.querySelector("#innerSignupModal");
+let usernameElement = formElement.querySelector("[name='username']");
+let passwordElement = formElement.querySelector("[name='password']");
+
+formElement.addEventListener("submit", (e) => {
+  localStorage.setItem("username", usernameElement.value);
+  localStorage.setItem("password", passwordElement.value);
+  window.location.reload();
+});
