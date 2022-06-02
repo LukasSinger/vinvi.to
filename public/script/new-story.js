433749d3682892d1f087a745bb6090458403fc33
itshoozi <1002619@isd535.org>
2022-06-02 12:33:57 -0500

    Added some finishing shine to the site

--- public/script/new-story.js
+++ public/script/new-story.js
@@ -4,3 +4,12 @@ let passwordElement = storyFormElement.querySelector("[name='password']");
 
 usernameElement.setAttribute("value", localStorage.getItem("username"));
 passwordElement.setAttribute("value", localStorage.getItem("password"));
+
+
+function richText() {
+    phrase = document.getElementById("soon");
+    phrase.classList.remove("translate-x-96");
+    setTimeout(function () {
+        phrase.classList.add("translate-x-96");
+      }, 1000);
+}
