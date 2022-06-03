c68e3d0b530e359098ad8fd7dd54a57470cf3c70
itshoozi <1002619@isd535.org>
2022-06-03 11:41:44 -0500

    Added fun ads and analytics

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: ["public/pages/**/*.{html,js}", "public/partials/signup.ejs", "public/partials/login.ejs", "public/partials/topbar.ejs", "public/partials/storyTile.ejs", "public/partials/settings.ejs", "public/partials/loggedInSidebar.ejs", "public/partials/loggedOutSidebar.ejs"],
+  content: ["public/pages/**/*.{html,js}", "public/partials/signup.ejs", "public/partials/advertisement.ejs", "public/partials/login.ejs", "public/partials/topbar.ejs", "public/partials/storyTile.ejs", "public/partials/settings.ejs", "public/partials/loggedInSidebar.ejs", "public/partials/loggedOutSidebar.ejs"],
   theme: {
     extend: {
       colors: {
         accent: "rgb(132 204 22)"
       },
       aspectRatio: {
-        169: "9 / 13"
+        169: "9 / 13",
+        102: "10 / 2"
       },
       fontFamily: {
         titles: ["Righteous", "cursive"]
